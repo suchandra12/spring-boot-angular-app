4cd6156f54820f755cb50e0779f6d772ffd50629
suchandra12 <suchandrareddy12@gmail.com>
2024-12-04 07:47:43 +0530

    Initial commit: Spring Boot + Angular application

---
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Item;
+import com.example.demo.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping
+    public List<Item> getAllItems() {
+        return itemService.getAllItems();
+    }
+
+    @PostMapping
+    public Item createItem(@RequestBody Item item) {
+        return itemService.createItem(item);
+    }
+}
---
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Item;
+import com.example.demo.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item createItem(Item item) {
+        return itemRepository.save(item);
+    }
+}
